698ddea8f345fe7ca55df72fdf3a9951c7549c7e
Azan Adnan <azanbutt2311@gmail.com>
2024-05-22 00:22:12 +0500

    Update validateUserOp function to use userOpHash for message hashing

--- scripts/execute.js
+++ scripts/execute.js
@@ -34,17 +34,17 @@ async function main() {
   //and rest is going to be the init calldata thats going to sent over to smart acc factory
 
   // call data in this is the call data of the transection. this is the call data staring from the smart account on. what do u wana do with the smart account
-  const initCode = "0x";
-  // FACTORY_ADDRESS +
-  // AccountFactory.interface
-  //   .encodeFunctionData("createAccount", [signer0Address])
-  //   .slice(2);
+  const initCode = //"0x";
+    FACTORY_ADDRESS +
+    AccountFactory.interface
+      .encodeFunctionData("createAccount", [signer0Address])
+      .slice(2);
 
   console.log("Sender: ", { sender });
 
-  // await entryPoint.depositTo(PM_ADDRESS, {
-  //   value: hre.ethers.parseEther("100"),
-  // });
+  await entryPoint.depositTo(PM_ADDRESS, {
+    value: hre.ethers.parseEther("100"),
+  });
 
   const Account = await hre.ethers.getContractFactory("Account");
 
@@ -59,8 +59,11 @@ async function main() {
     maxFeePerGas: hre.ethers.parseUnits("10", "gwei"),
     maxPriorityFeePerGas: hre.ethers.parseUnits("5", "gwei"),
     paymasterAndData: PM_ADDRESS,
-    signature: signer0.signMessage(hre.ethers.getBytes(hre.ethers.id("wee"))),
+    signature: "0x",
   };
+
+  const userOpHash = await entryPoint.getUserOpHash(userOp);
+  userOp.signature = await signer0.signMessage(hre.ethers.getBytes(userOpHash));
 
   const tx = entryPoint.handleOps([userOp], signer0Address);
   // const recipt = await tx.wait();
