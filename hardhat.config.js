550664a04eb3809bb021f4f344061e8f91a90993
Azan Adnan <azanbutt2311@gmail.com>
2024-05-23 00:45:51 +0500

    Update hardhat.config.js and package.json

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,9 +3,13 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
-  defaultNetwork: "localhost",
-
+  defaultNetwork: "arb",
+  networks: {
+    sepolia: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
   solidity: {
     version: "0.8.19",
     settings: {
